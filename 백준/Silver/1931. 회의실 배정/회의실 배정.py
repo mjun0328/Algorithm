d24330d26682da852265dd3a28273f6f21898f05
Kim Minjun <me@etimel.dev>
2024-11-01 17:11:05 +0900

    [Silver I] Title: 회의실 배정, Time: 2740 ms, Memory: 59852 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,13 @@
-def solution(N, tasks):
+def solution(tasks):
   # 종료 시각이 이른 순, 동 순위에서는 시작 시각이 이른 순으로 정렬
-  tasks = sorted(tasks, key=lambda task: task[0])
-  tasks = sorted(tasks, key=lambda task: task[1])
+  tasks = sorted(tasks, key=lambda task: (task[1], task[0]))
 
-  cnt = 0
-  time = 0
+  cnt = 0  # 진행된 회의의 개수
+  time = 0 # 마지막 회의의 종료 시각
   # 이전 회의 시간을 침해하지 않는 선에서 가장 종료 시간이 빠른 회의 선택(회의를 오래 하는 게 아닌 많이 하는 게 중요하기 때문)
-  for i in range(N):
-    task = tasks[i]
-    if time <= task[0]:
-      time = task[1]
+  for (start, end) in tasks:
+    if time <= start:
+      time = end
       cnt += 1
   return cnt
 
@@ -20,4 +18,4 @@ tasks = []
 for _ in range(N):
   tasks.append(list(map(int, input().split(' '))))
 
-print(solution(N, tasks))
+print(solution(tasks))
